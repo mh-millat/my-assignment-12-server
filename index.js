e0325cade1a247bb07c341208fb58406957dff46
Millat Sarker <millatsarker224@gmail.com>
2025-07-26 11:59:36 +0600

    added the second code

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors({
+    origin: ['http://localhost:5173'], // তোমার ফ্রন্টএন্ড URL
+    credentials: true
+}));
+app.use(express.json());
+
